eca0e60cfb6eb6286291e77ce5b8996a694aab0a
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-05-16 16:11:40 +0545

    Added oil for directory management

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,9 @@
+-- Oil.nvim
+-- https://github.com/stevearc/oil.nvim
+
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
