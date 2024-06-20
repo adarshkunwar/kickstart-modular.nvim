dcb8be7ffdf0e61e4fbb078dd40152af05414f5e
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-06-20 16:43:16 +0545

    Changed the neotree keymap to <leader>p instead of op

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { 'op', ':Neotree toggle<CR>', { desc = 'NeoTree toggle' } },
+    { '<leader>p', ':Neotree toggle<CR>', { desc = 'NeoTree toggle' } },
   },
   opts = {
     filesystem = {
