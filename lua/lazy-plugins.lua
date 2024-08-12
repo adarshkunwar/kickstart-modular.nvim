368ceac215b0011e3e284059524f60fd8d1f981b
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-08-12 12:47:57 +0545

    Added Prettier

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -12,8 +12,12 @@
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  -- 'github/copilot.vim',
+  'github/copilot.vim',
   'junegunn/goyo.vim',
+
+  'neovim/nvim-lspconfig',
+  'jose-elias-alvarez/null-ls.nvim',
+  'MunifTanjim/prettier.nvim',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
